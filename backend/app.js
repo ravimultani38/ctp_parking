44dab9a7df9b69adb9a494fbcb97d3068a1d3e23
Harpreet Singh <44241684+ravimultani38@users.noreply.github.com>
2024-12-11 16:06:16 -0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const { Server } = require("socket.io");
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // Frontend URL
+    origin: "*", // Frontend URL
     methods: ["GET", "POST"],
   },
 });
